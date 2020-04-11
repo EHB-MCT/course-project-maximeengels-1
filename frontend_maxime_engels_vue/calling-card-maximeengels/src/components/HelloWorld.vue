d7e778e13d8af66f72d2ac1c67f2e73de00eb2c5
Maxime Engels <maxime.engels@student.ehb.be>
2020-04-11 21:49:12 +0200

    First commit, animejs linked to vue

---
+++ frontend_maxime_engels_vue/calling-card-maximeengels/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div>Hello world</div>
+</template>
+
+<script>
+import anime from 'animejs/lib/anime.es.js';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+anime({
+  targets: 'div',
+  translateX: 250,
+  rotate: '1turn',
+  backgroundColor: '#FFF',
+  duration: 800,
+  loop: true
+});
+</script>
+
+
+
+<style scoped>
+
+</style>
