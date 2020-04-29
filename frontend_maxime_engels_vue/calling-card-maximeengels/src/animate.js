aab30a86c28b1da401b29695c42485b23018f678
Maxime Engels <maxime.engels@student.ehb.be>
2020-04-29 21:04:52 +0200

    name and illustration line animation (WIP)

--- frontend_maxime_engels_vue/calling-card-maximeengels/src/animate.js
+++ frontend_maxime_engels_vue/calling-card-maximeengels/src/animate.js
@@ -3,6 +3,18 @@ import anime from 'animejs';
 export function translate(element) {
     anime({
       targets: element,
-      translateX: 500
+      translateX: 500,
+      direction: 'alternate'
     });
+  }
+
+  export function linedraw() {
+    anime({
+        targets: ['#name-svg path', '.cls-1', '.cls-2'],
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutQuad',
+        duration: 6000,
+        direction: 'alternate',
+        loop: true
+      });
   }
